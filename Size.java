8ea36bad9f0fc7c3a5fddc20b4c5069bdd745070
flowell <miexihaha@outlook.com>
2016-11-22 18:01:29 +0800

    Create Size.java

---
+++ Size.java
@@ -0,0 +1,26 @@
+package APP;
+
+public 	class Size {
+	  
+    /* 
+     *设置棋盘网格行数列数为15 
+     */  
+    public static final int ROW = 16;  
+    public static final int COLUMN = 16;  
+      
+    /* 
+     * 设置棋盘初始位置坐标 
+     */  
+    public static final int X = 30;  
+    public static final int Y = 60;  
+      
+    /* 
+     * 设置棋子大小 
+     */  
+    public static final int CHESS_SIZE = 40;  
+      
+    /* 
+     * 设置棋盘格子大小 
+     */  
+    public static final int DISTANCE = 40; 
+}
